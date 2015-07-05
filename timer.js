61297f8ef943cdd24d8c805ba5345d08f3a247a7
wengjiaojiao <wengjiaojiao@outlook.com>
2015-07-05 23:48:52 +0800

    create timer function

--- timer.js
+++ timer.js
@@ -3,7 +3,7 @@ function CountDown() {
   if(maxtime>=0){
     minutes = Math.floor(maxtime/60);
     seconds = Math.floor(maxtime%60);
-    msg = "距离结束还有"+minutes+"分"+seconds+"秒";
+    msg = "时间：距离结束还有"+minutes+"分"+seconds+"秒";
     document.all["timer"].innerHTML=msg;
     if(maxtime == 5*60) {
       alert('注意，还有5分钟!');
@@ -16,6 +16,3 @@ function CountDown() {
 }
 
 timer = setInterval("CountDown()",1000);
-
-
-<div id="timer" style="color:red"></div>
